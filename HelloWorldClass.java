5ddc0bd505b57587e4b03cdc61433136f2238931
Emmanuel Bassey <basenetx@gmail.com>
2018-06-17 17:04:50 -0400

    Hi i'm just adding a demo source code

---
+++ HelloWorldClass.java
@@ -0,0 +1,9 @@
+public class HelloWorldClass 
+{
+  // Entry point for the program
+  public static void main(String args[])
+{
+   // prints "Hello World" on to the console
+   System.out.println("Hello World");
+}
+ }
