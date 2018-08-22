bbcd6efbeee359000d6daf162c9c135bd4e0ca35
Emmanuel Bassey <basenetx@gmail.com>
2018-08-21 21:53:14 -0400

    Abstract Class Added

---
+++ Abstract_Class/src/KikiPhones.java
@@ -0,0 +1,12 @@
+
+public abstract class KikiPhones extends MoyoPhones
+{
+public void flash() 
+{
+System.out.println("my phone should be able to flash");	
+}
+public void browse() 
+{
+System.out.println("the phone browser should work well");	
+}
+}
---
+++ Abstract_Class/src/MoyoPhones.java
@@ -0,0 +1,8 @@
+
+public abstract class MoyoPhones extends OrePhones
+{
+public void text() 
+{
+System.out.println("Moyo's phone can send text message");	
+}
+}
