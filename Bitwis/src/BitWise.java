2e764b564ecab6493a1b17c8bc7f8076a32062c9
Emmanuel Bassey <basenetx@gmail.com>
2018-08-22 06:31:55 -0400

    Bitwise Operator

---
+++ Bitwis/src/BitWise.java
@@ -0,0 +1,13 @@
+
+	public class BitWise 
+	{
+	public static void main(String[] args) 
+	{
+	int i = 25;
+	int j = 15;
+	int r = i|j;
+	int s = i&j;
+	System.out.println(r);
+	System.out.println(s);
+	}
+}
