56c02d04348f3b5bbf26060bb15ce29af440ac18
Emmanuel Bassey <basenetx@gmail.com>
2018-12-15 13:58:37 -0500

    Self_Driving_Car part 1 Demo is added.

---
+++ Self_Driving_Car/src/self_Driving_Car.java
@@ -0,0 +1,26 @@
+
+public class self_Driving_Car {
+
+	public static void main(String[] args) {
+		
+		//boolean isTrafficLightGreen = false;
+		
+		boolean isTrafficLightGreen = true;
+		
+		if(isTrafficLightGreen) {
+			
+			//traffic light is green
+			
+			double carSpeed = 60;
+			
+			System.out.println("The car will drive at a speed of " + carSpeed + " km/hr " );
+			
+		} else {
+			
+			System.out.println("The car will automatically stop.");
+		}
+			
+
+	}
+
+}
---
+++ Self_Driving_Car/src/self_Driving_Car2.java
@@ -0,0 +1,26 @@
+
+public class self_Driving_Car2 {
+
+	public static void main(String[] args) {
+		
+		boolean Traffic_Light_Is_Green = false;
+		
+		boolean Traffic_Light_Is_Red = false;
+		
+		if(Traffic_Light_Is_Green) {
+			
+			//traffic light changed to green
+			
+			System.out.println("The car should drive");
+			
+		} else if(Traffic_Light_Is_Red) {
+			
+			System.out.println("The car should stop.");
+			
+		} else {
+		
+		System.out.println("Car slow down and stop after beep sound");
+			
+	}
+	}
+}
