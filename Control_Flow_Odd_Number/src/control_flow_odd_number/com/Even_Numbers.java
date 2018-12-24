462892aefb5bb1d2f3323f162780c2e361c1500f
Emmanuel Bassey <basenetx@gmail.com>
2018-12-23 21:47:13 -0500

    This code print both odd numbers and even numbers between 0 and 100.

---
+++ Control_Flow_Odd_Number/src/control_flow_odd_number/com/Even_Numbers.java
@@ -0,0 +1,17 @@
+package control_flow_odd_number.com;
+
+public class Even_Numbers {
+
+	public static void main(String[] args) throws Exception{
+					
+		for( int even_Number = 2 ; even_Number < 100; even_Number = even_Number + 2) {
+
+		if( even_Number < 100 ) {
+				
+		System.out.println(even_Number);
+					           
+					 }
+				 }
+			}
+
+		}
---
+++ Control_Flow_Odd_Number/src/control_flow_odd_number/com/Test_For_Odd_Numbers.java
@@ -0,0 +1,17 @@
+package control_flow_odd_number.com;
+
+public class Test_For_Odd_Numbers {
+
+	public static void main(String[] args) throws Exception{
+			
+			 for( int oddNumber = 1 ; oddNumber < 100; oddNumber = oddNumber + 2) {
+
+			if( oddNumber < 100 ) {
+		
+			System.out.println(oddNumber);
+			           
+			 }
+		 }
+	}
+
+}
